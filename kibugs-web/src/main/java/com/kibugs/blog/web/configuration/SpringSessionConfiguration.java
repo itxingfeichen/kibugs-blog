6a35516301f0081da42789f957a31a3db10cb106
chenxingfei <chenxingfei@mintechai.com>
2019-11-03 23:12:55 +0800

    博客页面提交

--- kibugs-web/src/main/java/com/kibugs/blog/web/configuration/SpringSessionConfiguration.java
+++ kibugs-web/src/main/java/com/kibugs/blog/web/configuration/SpringSessionConfiguration.java
@@ -1,18 +1,20 @@
-//package com.kibugs.blog.web.configuration;
-//
-//import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-//
-///**
-// * @author : chenxingfei
-// * @date: 2019-11-03  22:44
-// * @description: spring-session-data
-// */
-//@EnableRedisHttpSession(redisNamespace = "kibugs:session")
-//public class SpringSessionConfiguration {
-//
-////    @Bean
-////    public LettuceConnectionFactory connectionFactory() {
-////        return new LettuceConnectionFactory();
-////    }
-//
-//}
+package com.kibugs.blog.web.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+/**
+ * @author : chenxingfei
+ * @date: 2019-11-03  22:44
+ * @description: spring-session-data
+ */
+@EnableRedisHttpSession(redisNamespace = "kibugs:session")
+public class SpringSessionConfiguration {
+
+    @Bean
+    public LettuceConnectionFactory connectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+}
