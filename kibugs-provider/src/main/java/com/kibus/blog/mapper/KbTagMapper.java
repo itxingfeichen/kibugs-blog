20e22b5e79e165e4a1274be93ceb1bb33eb04718
chenxingfei <chenxingfei@mintechai.com>
2019-11-23 07:43:53 +0800

    博客功能完善（加载更多）

--- kibugs-provider/src/main/java/com/kibus/blog/mapper/KbTagMapper.java
+++ kibugs-provider/src/main/java/com/kibus/blog/mapper/KbTagMapper.java
@@ -21,7 +21,7 @@ public interface KbTagMapper extends BaseMapper<KbTag> {
      * 获取top10标签
      * @return
      */
-    @Select({"select tag.name,count(kbt.tag_id) num from kb_blog_tag kbt left join kb_blog kb on kbt.blog_id=kb.id left join kb_tag tag on kbt.tag_id = tag.id where kb.delete_status=0 and kb.publish_status=1  group by kbt.tag_id limit 10;"})
+    @Select({"select tag.id id,tag.name,count(kbt.tag_id) num from kb_blog_tag kbt left join kb_blog kb on kbt.blog_id=kb.id left join kb_tag tag on kbt.tag_id = tag.id where kb.delete_status=0 and kb.publish_status=1  group by kbt.tag_id limit 10;"})
     List<Map<String,Integer>> getTagsForTop10();
     /**
      * 获取所有标签
