52e16fd388baf7ffbe7c7f619196535679db6a46
chenxingfei <chenxingfei@mintechai.com>
2019-11-22 08:02:48 +0800

    博客功能完善（加载更多）

--- kibugs-web/src/main/java/com/kibugs/blog/web/web/KiBlogIndexController.java
+++ kibugs-web/src/main/java/com/kibugs/blog/web/web/KiBlogIndexController.java
@@ -49,47 +49,9 @@ public class KiBlogIndexController extends BaseController {
 
     @RequestMapping("loadMore")
     @ResponseBody
-    public CommonResponse loadMore(Integer current, String type) {
+    public CommonResponse loadMore(Integer current, String type, Long objectId) {
         log.info("params={},{}", current, type);
-        String data = "<div class=\"ui padded segment vertical ki-padded-tb-large ki-padded-lr-response ki-padded-tb-large ki-padded-lr-clear\">\n" +
-                "                        <div class=\"ui mobile reversed stackable grid\">\n" +
-                "                            <div class=\"eleven wide column\">\n" +
-                "                                <h3 class=\"ui header\">\n" +
-                "                                    <a href=\"/blog/detail/1\" class=\"header\">标题</a></h3>\n" +
-                "                                <p class=\"ki-text\">昨天，35岁的表姐把我拉黑了。 表姐是医院有名的好护士，脾气好，耐心足，对谁都笑眯眯。 最近小半年，他们医院在搞评职称，指标之一就是护士的论文发布情况。 得知消息后，表姐开始着手发布职称论文，与此同时，我的噩梦也来了。 获得授权，非商业转载请注明出处。......</p>\n" +
-                "                                <div class=\"ui stackable  grid\">\n" +
-                "                                    <div class=\"eleven wide column\">\n" +
-                "                                        <div class=\"ui horizontal link list mini\">\n" +
-                "                                            <div class=\"item\">\n" +
-                "                                                <img src=\"https://picsum.photos/id/1011/367/267\" alt=\"头像地址\" class=\"ui avatar image\">\n" +
-                "                                                <div class=\"content inverted\">\n" +
-                "                                                    <a href=\"#\" class=\"header\">爱折腾</a>\n" +
-                "                                                </div>\n" +
-                "                                            </div>\n" +
-                "                                            <div class=\"item\">\n" +
-                "                                                <i class=\"calendar icon\"></i><span>2019-11-02</span>\n" +
-                "                                            </div>\n" +
-                "                                            <div class=\"item\">\n" +
-                "                                                <i class=\"eye icon\"></i><span>123</span>\n" +
-                "                                            </div>\n" +
-                "                                            \n" +
-                "                                        </div>\n" +
-                "                                    </div>\n" +
-                "                                    <div class=\"right aligned ui five wide column\">\n" +
-                "                                        <a href=\"#\" class=\"ui teal basic label ki-padded-tiny ki-text-thin\">核心技术</a>\n" +
-                "                                    </div>\n" +
-                "\n" +
-                "                                </div>\n" +
-                "\n" +
-                "                            </div>\n" +
-                "                            <div class=\"ui five wide column\">\n" +
-                "                                <a href=\"@{/blog/detail/{id}(id=${blog.id})}\" target=\"_blank\">\n" +
-                "                                    <img src=\"https://picsum.photos/seed/picsum/800/450\" alt=\"\" class=\"ui rounded image ki-padded-tb-big ki-text-thin\" style=\"width:800px;height:200px\">\n" +
-                "                                </a>\n" +
-                "\n" +
-                "                            </div>\n" +
-                "                        </div>\n" +
-                "                    </div>";
+        String data = new StringBuilder().append("<div class=\"ui padded segment vertical ki-padded-tb-large ki-padded-lr-response ki-padded-tb-large ki-padded-lr-clear\">\n").append("<div class=\"ui mobile reversed stackable grid\">\n").append("<div class=\"eleven wide column\">\n").append("<h3 class=\"ui header\">\n").append("<a href=\"/blog/detail/1\" class=\"header\">标题</a></h3>\n").append("<p class=\"ki-text\">昨天，35岁的表姐把我拉黑了。 表姐是医院有名的好护士，脾气好，耐心足，对谁都笑眯眯。 最近小半年，他们医院在搞评职称，指标之一就是护士的论文发布情况。 得知消息后，表姐开始着手发布职称论文，与此同时，我的噩梦也来了。 获得授权，非商业转载请注明出处。......</p>\n").append("<div class=\"ui stackable  grid\">\n").append("<div class=\"eleven wide column\">\n").append("<div class=\"ui horizontal link list mini\">\n").append("<div class=\"item\">\n").append("<img src=\"https://picsum.photos/id/1011/367/267\" alt=\"头像地址\" class=\"ui avatar image\">\n").append("<div class=\"content inverted\">\n").append("<a href=\"#\" class=\"header\">爱折腾</a>\n").append("</div>\n").append("</div>\n").append("<div class=\"item\">\n").append("<i class=\"calendar icon\"></i><span>2019-11-02</span>\n").append("</div>\n").append("<div class=\"item\">\n").append("<i class=\"eye icon\"></i><span>123</span>\n").append("</div>\n").append("\n").append("</div>\n").append("</div>\n").append("<div class=\"right aligned ui five wide column\">\n").append("<a href=\"#\" class=\"ui teal basic label ki-padded-tiny ki-text-thin\">核心技术</a>\n").append("</div>\n").append("\n").append("</div>\n").append("\n").append("</div>\n").append("<div class=\"ui five wide column\">\n").append("<a href=\"@{/blog/detail/{id}(id=${blog.id})}\" target=\"_blank\">\n").append("<img src=\"https://picsum.photos/seed/picsum/800/450\" alt=\"\" class=\"ui rounded image ki-padded-tb-big ki-text-thin\" style=\"width:800px;height:200px\">\n").append("</a>\n").append("\n").append("</div>\n").append("</div>\n").append("</div>").toString();
 
         return CommonResponse.success(data);
     }
--- kibugs-web/src/main/resources/static/js/index.js
+++ kibugs-web/src/main/resources/static/js/index.js
@@ -4,14 +4,16 @@ Index = {
      * 加载更多
      * @param current
      */
-    loadMore: function (current, type) {
+    loadMore: function (type) {
         $("#loadMoreId").addClass("loader")
         $("#loadMoreId").text("加载中...");
-        $("#loadMoreId").attr("disabled",true);
-        $.get("/loadMore", {'current': current + 1, 'type': type}, function (res) {
+        $("#loadMoreId").attr("disabled", true);
+        var current = $("#current").val();
+        var objectId = $("#objectId").val();
+        $.get("/loadMore", {'current': current + 1, 'type': type, 'objectId': objectId}, function (res) {
             $("#data-content").append(res.data);
             $("#loadMoreId").removeClass("loader")
-            $("#loadMoreId").attr("disabled",false);
+            $("#loadMoreId").attr("disabled", false);
             $("#loadMoreId").text("加载更多");
         })
 
